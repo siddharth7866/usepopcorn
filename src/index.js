0a883bceee457a39b33204f45e7fbd0537dd2054
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-09-20 15:33:08 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+// import App from "./App";
+// import "./index.css";
+import StarRating from "./starrating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<StrictMode>{<StarRating />}</StrictMode>);
